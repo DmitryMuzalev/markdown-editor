75687b8a63d087c54ee2389bba770f2756a6ab26
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2024-09-16 14:16:23 +0300

    feat: create Documents component

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,17 +1,17 @@
 //_Styles:
-import styles from './Sidebar.module.scss';
+import styles from "./Sidebar.module.scss";
 
 //_Components:
-import { Logo } from '../Logo/Logo';
-import { DocumentsList } from './DocumentsList/DocumentsList';
-import { ThemSwitcher } from './ThemSwitcher/ThemSwitcher';
+import { Logo } from "../Logo/Logo";
+import { Documents } from "./Documents/Documents";
+import { ThemSwitcher } from "./ThemSwitcher/ThemSwitcher";
 
 function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <Logo />
       <p className="section-title">my documents</p>
-      <DocumentsList classes={[styles.sidebarDocumentsList]} />
+      <Documents classes={[styles.sidebarDocuments]} />
       <ThemSwitcher />
     </div>
   );
--- src/components/Sidebar/ThemSwitcher/ThemSwitcher.jsx
+++ src/components/Sidebar/ThemSwitcher/ThemSwitcher.jsx
@@ -1,31 +1,41 @@
-import styles from './ThemSwitcher.module.scss';
-import { useState, useEffect } from 'react';
+import styles from "./ThemSwitcher.module.scss";
+import { useState, useEffect } from "react";
 
-import clsx from 'clsx';
+import clsx from "clsx";
 
 function ThemSwitcher() {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState("light");
 
   useEffect(() => {
-    document.body.setAttribute('data-theme', theme);
+    document.body.setAttribute("data-theme", theme);
   }, [theme]);
 
   const toggleSwitcher = () => {
-    setTheme(theme === 'dark' ? 'light' : 'dark');
+    setTheme(theme === "dark" ? "light" : "dark");
   };
 
   const switcherStyles = clsx(
     styles.switcher,
-    theme === 'dark' && styles.switcherChecked
+    theme === "dark" && styles.switcherChecked
   );
 
   return (
     <div className={styles.themeSwitcher}>
-      <i className="icon-light-mode"></i>
+      <i
+        className="icon-light-mode"
+        style={{
+          color: theme === "dark" ? "var(--natural-500)" : "var(--white)",
+        }}
+      ></i>
       <button className={switcherStyles} onClick={toggleSwitcher}>
         <span className={styles.switcherSlider}></span>
       </button>
-      <i className="icon-dark-mode"></i>
+      <i
+        className="icon-dark-mode"
+        style={{
+          color: theme === "dark" ? "var(--white)" : "var(--natural-500)",
+        }}
+      ></i>
     </div>
   );
 }
