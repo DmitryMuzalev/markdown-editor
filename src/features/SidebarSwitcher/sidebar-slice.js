668f0782cf76a67020087a88e8a152dd4c6d24d8
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2024-09-25 23:49:52 +0300

    fix: fix Sidebar

--- src/features/SidebarSwitcher/sidebar-slice.js
+++ src/features/SidebarSwitcher/sidebar-slice.js
@@ -1,10 +1,16 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { createDocument, selectDocument } from '../Documents/documents-slice';
 
 const sidebarSlice = createSlice({
   name: 'showSidebar',
   initialState: false,
   reducers: {
     toggleShowSidebar: (state) => !state,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(selectDocument, () => false)
+      .addCase(createDocument, () => false);
   },
 });
 
